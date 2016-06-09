cef83a611848373440fb2480303edf929e8671b5
JessicaHerford <jrherford12@gmail.com>
2016-06-09 17:23:27 -0600

    Added button to repeat function

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 var cube = $(".cube").cube();
 cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+
+$('button').click(function(){
+  cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+})
