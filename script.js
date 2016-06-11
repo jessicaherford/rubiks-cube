a17984f64412bb301d96ffe914f277a2ca3c61cd
JessicaHerford <jrherford12@gmail.com>
2016-06-10 20:17:36 -0600

    Working on disabling button

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var cube = $(".cube").cube();
+
 cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
 
-$('button').click(function(){
-  cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
-})
+
+// $('button').click(function(){
+//   cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+// })
+
+var button = document.querySelector("button");
+  function once() {
+    while(cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'")){
+    button.removeEventListener("click", once);
+  }
+  }
+  button.addEventListener("click", once);
