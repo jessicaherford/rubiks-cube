7cfd0db7e639db905d390ce91b817d1a447fdea9
JessicaHerford <jrherford12@gmail.com>
2016-06-10 21:39:21 -0600

    breaking things

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 var cube = $(".cube").cube();
 
-cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+// cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
 
+var button = document.getElementById('againButton');
 
-// $('button').click(function(){
-//   cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
-// })
+$('button').click(function(){
+  $('button').hide();
+})
 
-var button = document.querySelector("button");
-  function once() {
-    while(cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'")){
-    button.removeEventListener("click", once);
-  }
-  }
-  button.addEventListener("click", once);
+$(function(){
+  $('button').hide();
+  cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+  $('button').show();
+});
