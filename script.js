43d35a718913a8adeb16e4a0bb3a756966868e6f
JessicaHerford <jrherford12@gmail.com>
2016-06-11 22:41:22 -0600

    Working on disabling button while function is running

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ var cube = $(".cube").cube();
 
 var button = document.getElementById('againButton');
 
-$('button').click(function(){
-  $('button').hide();
-})
+
 
 $(function(){
-  $('button').hide();
+  $('button').attr("disabled", true);
   cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
-  $('button').show();
+
+
+
 });
+
+$('button').click(function(){
+  $('button').attr("disabled", false);
+  cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+})
+ // $("#ajaxStart").attr("disabled", true);
