6175b3e5ed331c9bdbcc641736353b0b5fa7fc65
JessicaHerford <jrherford12@gmail.com>
2016-06-12 18:59:33 -0600

    buttons

--- script.js
+++ script.js
@@ -5,17 +5,43 @@ var cube = $(".cube").cube();
 var button = document.getElementById('againButton');
 
 
+//
+// $(function(){
+//   $('button').attr("disabled", true);
+//   cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+//
+// $('button').attr("disabled", false);
+//  // $("#ajaxStart").attr("disabled", true);
+// });
+//
+// $('button').click(function(){
+//   cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+// })
 
-$(function(){
-  $('button').attr("disabled", true);
-  cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+$(function() {
+
+    jQuery.fn.extend({
+        disable: function(state) {
+            return this.each(function() {
+                this.disabled = state;
+            });
+        }
+    });
+
+    // $("#deliveryNext").is(":disabled")
+
+    $(function(){
+      cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+      $('button').is(":disabled")
 
 
-
+    $('button').click(function(){
+      cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+    })
+    });
 });
-
-$('button').click(function(){
-  $('button').attr("disabled", false);
-  cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
-})
- // $("#ajaxStart").attr("disabled", true);
+//
+//
+// $('button').disable(true);
+// cube.execute("x (R' U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 (R U R') D2 (R U' R') D2 R2 x'");
+// $('button').disable(false);
